7fbe495fcadff26b36e4c9f8f2641f137dbdb084
lil-works <lilian@lil-works.com>
2017-04-26 12:02:46 +0200

    some translate

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
             new JMS\TranslationBundle\JMSTranslationBundle(),
+            new Lexik\Bundle\TranslationBundle\LexikTranslationBundle(),
             new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
             new Sonata\SeoBundle\SonataSeoBundle(),
             new Liip\ThemeBundle\LiipThemeBundle(),
